741646cc616e5c374159c0c88b63b4c5344161f6
Ali07117 <alirashid07117@gmail.com>
2024-07-14 13:40:55 +0500

    update

--- src/Components/HoverComponent.js
+++ src/Components/HoverComponent.js
@@ -6,7 +6,7 @@ function HoverComponent(props) {
          <div className="elem overflow-hidden flex justify-between items-center  relative border-b border-[#d3adad] py-[20px]">
      <div className="elem-layer h-[100%] w-[100%] absolute translate-y-[-100%] z-[-1] duration-200  bg-[#FF9831]"></div>
      <span className='flex justify-between items-center w-[100%] px-[30px]'>
-     <p className='text-[55px] hover-text font-[700]'>{props.title}</p>
+     <p className='text-[55px] hover-text font-[700] truncate'>{props.title}</p>
      <span className='flex flex-col items-end text-right'>
       <p className='hover-text-brand'>{props.brand}</p>
       <p className='text-[#00000066] hover-text-brand'>{props.text}</p>
